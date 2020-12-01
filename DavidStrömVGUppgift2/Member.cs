89511a32f6fd8b1595e3cd0b33b0416abcc0ae95
David Ström <david.strom88@gmail.com>
2020-12-01 22:27:51 +0100

    Appen funkar som den ska.

---
+++ DavidStrömVGUppgift2/Member.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DavidStrömVGUppgift2
+{
+    class Member
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public int Age { get; set; }
+        public string Hobby { get; set; }
+        public string FavoriteFood { get; set; }
+        public string FavoriteColor { get; set; }
+        public string Motivation { get; set; }
+        public string HomeTown { get; set; }
+        public string Birthplace { get; set; }
+        public int Siblings { get; set; }
+        public Member(string name, int height, int age, string hobby,
+            string favoriteFood, string favoriteColor, string motivation,
+            string homeTown, string birthplace, int siblings)
+        {
+            Name = name;
+            Height = height;
+            Age = age;
+            Hobby = hobby;
+            FavoriteFood = favoriteFood;
+            FavoriteColor = favoriteColor;
+            Motivation = motivation;
+            HomeTown = homeTown;
+            Birthplace = birthplace;
+            Siblings = siblings;
+        }
+
+        public void Describe()
+        {
+            Console.WriteLine("{0} är {1} år gammal, {2} cm lång och har {3} som hobby.", Name, Age, Height, Hobby);
+            Console.WriteLine("{0}s favoritkäk är {1} och favoritfärgen är {2}.", Name, FavoriteFood, FavoriteColor);
+            Console.WriteLine("{0}s motivation till programering är {1}.", Name, Motivation);
+            Console.WriteLine("{0} föddes i {1} och bor nu i {2}", Name, Birthplace, HomeTown);
+            Console.WriteLine("{0} har {1} syskon.", Name, Siblings);
+        }
+    }
+}
--- DavidStrömVGUppgift2/Program.cs
+++ DavidStrömVGUppgift2/Program.cs
@@ -1,14 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace DavidStrömVGUppgift2
 {
     class Program
     {
+        static List<Member> group = new List<Member>();
         static void Main(string[] args)
         {
+            PopulateGroup("Elin", 170, 31, "hästar", "sushi", "röd", "personliga utveckling", "Knivsta", "Karlskoga", 2);
+            PopulateGroup("Cecilia", 163, 29, "The Sims", "risotto", "gul", "kreativitet", "Norrköping", "Norrköping", 1);
+            PopulateGroup("Jeremy", 181, 19, "gaming", "älggryta", "teal", "att få ett jobb", "Djurö", "Köln", 1);
+            PopulateGroup("Sanjin", 179, 30, "fotboll", "pizza", "blå", "att få ett jobb", "Norrköping", "Mostar", 2);
+            PopulateGroup("Oscar", 185, 26, "fotboll", "lasagne", "blå", "att få ett jobb", "Stockholm", "Stockholm", 1);
+            PopulateGroup("Johan", 194, 34, "gaming", "tacos", "blå", "en trygg framtid", "Mariefred", "Mariefred", 2);
+            PopulateGroup("David", 183, 32, "BJJ", "tacos", "blå", "problemlösning", "Norrtälje", "Göteborg", 1);
+
             //1.Programmet skall vid uppstart fråga användaren om en kod. Om 
             //denna kod är == med namnet på er basgrupp skall tillgång till 
             //resten av programmet ges.Annars skall ett passande meddelande skrivas ut.
+
+            int time = 2000;
+            Console.WriteLine("Välkommen till programmet!");
+            do
+            {
+                Console.Write("Ange koden: ");
+                string password = Console.ReadLine();
+                if (password == "Bästkusten")
+                {
+                    Console.WriteLine("Korrekt! Du angav rätt kod!");
+                    Thread.Sleep(time);
+                    Console.Clear();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Fel kod! Försök igen...");
+                    Thread.Sleep(time);
+                    Console.Clear();
+                } 
+            } while (true);
 
             //2.Användaren skall få olika val presenterat i form av en meny.
             //a.Lista alla deltagare i gruppen separerat med ,
@@ -16,6 +48,50 @@ namespace DavidStrömVGUppgift2
             //i.Varje deltagare skall ha en unik sträng som beskriver personens 
             //största driv till programmering.
             //c.Möjligheten att ta bort en person.
+            bool exit = false;
+            do
+            {
+                Menu();
+                int.TryParse(Console.ReadLine(), out int choice);
+                switch (choice)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Här är alla medlemmar i Bästkusten:");
+                        ShowMembers();
+                        Console.ReadKey(true);
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Vilken gruppmedlem vill du veta mer om?");
+                        ShowMembers(1);
+                        choice = UserChoice();
+                        Console.Clear();
+                        group[choice].Describe();
+                        Console.ReadKey(true);
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("Vilken gruppmedlem vill du ta bort?");
+                        ShowMembers(1);
+                        choice = UserChoice();
+                        Console.WriteLine("{0} är nu borttagen.", group[choice].Name);
+                        group.RemoveAt(choice);
+                        Thread.Sleep(time);
+                        break;
+                    case 4:
+                        Console.WriteLine("Programmet avslutas.");
+                        exit = true;
+                        Thread.Sleep(time);
+                        break;
+                    default:
+                        Console.WriteLine("Du måste vålja mellan 1-4.");
+                        Thread.Sleep(time);
+                        break;
+                }
+            } while (!exit);
+
+
             //3.Programmet skall versionshanteras och måste innehålla minst tre commits.
             //4.Koden skall innehålla relevanta kommentarer
 
@@ -29,6 +105,51 @@ namespace DavidStrömVGUppgift2
             //c.För och nackdelarna med tillvägagångssättet?
             //d.Varför valde du en for istället för en foreach?
             //e.Hur tänkte du vid namngivning?
+        }
+
+        static void PopulateGroup(string name, int height, int age, string hobby, 
+            string favoriteFood, string favoriteColor, string motivation, 
+            string homeTown, string birthplace, int siblings)
+        {
+            var member = new Member(name, height, age, hobby, favoriteFood, 
+                favoriteColor, motivation, homeTown, birthplace, siblings);
+            group.Add(member);
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Vad vill du göra?");
+            Console.WriteLine("1. Lista alla deltagare i gruppen.");
+            Console.WriteLine("2. Veta med om en specifik gruppmedlem.");
+            Console.WriteLine("3. Ta bort en person ur gruppen.");
+            Console.WriteLine("4. Avsluta programmet.");
+        }
+
+        static void ShowMembers()
+        {
+            for (int i = 0; i < group.Count; i++)
+            {
+                if (i == 0)
+                    Console.Write(group[i].Name);
+                else if (i + 1 == group.Count)
+                    Console.Write(" & " + group[i].Name + ".");
+                else
+                    Console.Write(", " + group[i].Name);
+            }
+        }
+
+        static void ShowMembers(int ctr)
+        {
+            foreach (var member in group)
+                Console.WriteLine("{0}. {1}", ctr++, member.Name);
+        }
+
+        static int UserChoice()
+        {
+            int.TryParse(Console.ReadLine(), out int choice);
+            choice--;
+            return choice;
         }
     }
 }
